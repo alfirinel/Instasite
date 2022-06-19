8311c5d25d390e9c1513dd7408efcd8f0fbbadb8
Alfirinel <dalnimdeluna@gmail.com>
2022-06-19 20:59:04 +0300

    template site

---
+++ views/pages/signup.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="/assets/style.css">
+<div class="design-form">
+    <img class="logo-form" src="/images/logo.png" alt="logo">
+    <form action="" method="" id="formSignUp">
+        <label for="login" class="item-text">Login:</label>
+        <input type="text" name="login" id="login" class="item"/>
+        <label for="pass" class="item-text">Password:</label>
+        <input type="password" name="password" id="pass" class="item"/>
+        <label for="pass" class="item-text">Password&nbspconfirm:</label>
+        <input type="password" name="password_confirm" id="pass" class="item"/>
+        <label for="login" class="item-text">Email:</label>
+        <input type="text" name="login" id="login" class="item"/>
+        <input type="submit" value="Sign Up" class="item-btn">
+    </form>
+</div>
---
+++ views/templates/header.php
@@ -0,0 +1,14 @@
+<div class="bg-image">
+    <div class="nav"><a href="index.php"><img class="logo" src="/images/logo.png" alt="logo"></a>
+
+        <ul class="header-list">
+            <li  class="header-list-item"><a href="index.php" class="header_name">Home</a></li>
+            <li  class="header-list-item"><a href="index.php" class="header_name">Gallery</a></li>
+        </ul>
+        <ul class="auth-list">
+            <li  class="auth-list-item"><a href="index.php" class="header_name">Login</a></li>
+            <li  class="auth-list-item">|</li>
+            <li  class="auth-list-item"><a href="index.php" class="header_name">Sign Up</a></li>
+        </ul>
+    </div>
+</div>
