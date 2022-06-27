be0bf4080e0436392ad4aadc546e1677598ef8d9
D2022V <p963215875@protonmail.com>
2022-06-27 13:01:48 +0300

    Route create

---
+++ app/core/Route.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace app\core;
+
+
+class Route
+{
+    static public function init()
+    {
+        $uri = $_SERVER['REQUEST_URI'];
+        $uriComponents = explode('/', $uri);
+        array_shift($uriComponents);
+        if (count($uriComponents) > 2) {
+            self::notFound();
+        }
+        $controllerName = 'index';
+        $actionName = 'index';
+        if (!empty($uriComponents[0])) {
+            $controllerName = strtolower($uriComponents[0]);
+        }
+        if (!empty($uriComponents[1])) {
+            $actionName = strtolower($uriComponents[1]);
+        }
+        $controllerClassName = '\app\controllers\\' . ucfirst($controllerName);
+        if (!class_exists($controllerClassName)) {
+            self::notFound();
+        }
+        $controller = new $controllerClassName();
+        if (!method_exists($controller, $actionName)) {
+            self::notFound();
+        }
+        $controller->$actionName();
+    }
+
+    static public function notFound()
+    {
+        http_response_code('404');
+        exit();
+    }
+
+    public static function url(string $action = 'index'): string
+    {
+        return "/?action=$action";
+    }
+
+    public static function redirect(string $action, $controller = 'index')
+    {
+        $url= "/$controller/$action";
+        var_dump($url);
+        header("Location: $url");
+        exit();
+    }
+
+}
