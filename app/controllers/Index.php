c06a94a84746e2cfa4009ab74dc45e0130760fbc
D2022V <p963215875@protonmail.com>
2022-06-27 13:02:57 +0300

    Index controller create

---
+++ app/controllers/Index.php
@@ -0,0 +1,112 @@
+<?php
+
+
+namespace app\controllers;
+
+
+use app\core\Route;
+use app\core\View;
+use app\helpers\Session;
+use app\helpers\Validator;
+use app\models\AuthModel;
+
+class Index
+{
+    protected View $view;
+
+    protected $authModel;
+
+    /**
+     * Controller constructor.
+     */
+    public function __construct()
+    {
+        $this->view = new View();
+        $this->authModel = new AuthModel();
+    }
+
+    public function index()
+    {
+        $this->view->render('home');
+    }
+
+    public function gallery()
+    {
+        $this->view->render('gallery');
+    }
+
+    public function login()
+    {
+
+        $this->view->render('login', [
+            'login' => Session::getValue('login'),
+            'errors' => Session::getErrors(),
+        ]);
+    }
+
+    public function checkLogin()
+    {
+        $login = filter_input(INPUT_POST, 'login');
+        $password = filter_input(INPUT_POST, 'password');
+        Validator::checkListInput([$login,$password]);
+        $isLogin = $this->authModel->getUserAndPass($login);
+        $realPass = password_verify($password, $isLogin['pass']);
+        if($realPass && $isLogin){
+            Session::delFromSession();
+            Session::setValue('login', $login);
+            Route::redirect('index');
+        }else{
+            Session::delFromSession('errors');
+            $errors[] = 'incorrect login or password!';
+            Session::setErrors($errors);
+            Session::setValue('login', $login);
+            Route::redirect('login');
+        }
+
+    }
+
+    public function signup()
+    {
+        $this->view->render('signup', [
+            'login' => Session::getValue('login'),
+            'name' => Session::getValue('name'),
+            'email' => Session::getValue('email'),
+            'errors' => Session::getErrors(),
+        ]);
+    }
+
+    public function checkSignup()
+    {
+        $name = filter_input(INPUT_POST, 'name');
+        $login = filter_input(INPUT_POST, 'login');
+        $email = filter_input(INPUT_POST, 'email');
+        $password = filter_input(INPUT_POST, 'password');
+        $conf_password = filter_input(INPUT_POST, 'password_confirm');
+        Validator::checkListInput([$name, $login, $email, $password]);
+        $cur_date = filter_input(INPUT_POST, 'cur_date');
+        $cur_date = date("d-M-Y H:i:s", $cur_date);
+        if (!Validator::equalPass($password, $conf_password)) {
+            Route::redirect('signup');
+        }
+        $hash_pass = password_hash($password, PASSWORD_BCRYPT);
+        session_start();
+        if (!empty($_SESSION['errors'])) {
+            Session::setValue('login', $login);
+            Session::setValue('name', $name);
+            Session::setValue('email', $email);
+            Route::redirect('signup');
+        } else {
+            $this->authModel->addUser($login, $hash_pass, $email, $name, $cur_date);
+            Session::delFromSession();
+            Route::redirect('index');
+        }
+    }
+
+    public function logout()
+    {
+        session_start();
+        session_destroy();
+        Route::redirect('index');
+    }
+
+}
