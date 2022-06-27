1f51e58c81e4b356e968435ae4e99b9218ec404b
D2022V <p963215875@protonmail.com>
2022-06-27 12:57:32 +0300

    correct style

--- app/views/pages/gallery.php
+++ app/views/pages/gallery.php
@@ -1,3 +1,2 @@
-<?php include_once self::PAGES_FOLDER.DIRECTORY_SEPARATOR.'includes\header.php';?>
 <span>working gallery</span>
-<?php include_once self::PAGES_FOLDER.DIRECTORY_SEPARATOR.'includes\footer.php';?>
+
--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -1,4 +1,2 @@
-<?php include_once self::PAGES_FOLDER.DIRECTORY_SEPARATOR.'includes\header.php';?>
 <span>working home</span>
 
-<?php include_once self::PAGES_FOLDER.DIRECTORY_SEPARATOR.'includes\footer.php';?>
