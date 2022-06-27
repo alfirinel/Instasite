94d35cb115fcd450c47584502d66e8e4dbdbca55
D2022V <p963215875@protonmail.com>
2022-06-27 12:55:08 +0300

    header.php with logic selected

--- app/views/pages/includes/header.php
+++ app/views/pages/includes/header.php
@@ -1,13 +1,20 @@
 <div class="bg-image">
-    <div class="nav"><a href="/"><img class="logo" src="app" alt="logo"></a>
+    <div class="nav"><a href="/"><img class="logo" src="/images/logo.png" alt="logo"></a>
+        <?php if(\app\helpers\Session::isAuth()):?>
+            <ul class="header-list">
+                <li  class="header-list-item"><a href="/" class="header_name">Home</a></li>
+                <li  class="header-list-item"><a href="/index/gallery" class="header_name">Gallery</a></li>
+            </ul>
+            <ul class="auth-list">
+                <li class="auth-list-item"><?= \app\helpers\Session::getValue('login')?>!</li>
+                <li class="auth-list-item"><a href="/index/logout">Log out</a></li>
+            </ul>
+        <?php else:?>
+            <ul class="auth-list">
+                <li  class="auth-list-item"><a href="/index/login" class="header_name">Login</a></li>
+                <li  class="auth-list-item"><a href="/index/signup" class="header_name">Register</a></li>
+            </ul>
+        <?php endif;?>
 
-        <ul class="header-list">
-            <li  class="header-list-item"><a href="/" class="header_name">Home</a></li>
-            <li  class="header-list-item"><a href="gallery.php" class="header_name">Gallery</a></li>
-        </ul>
-        <ul class="auth-list">
-            <li  class="auth-list-item"><a href="index/login" class="header_name">Login</a></li>
-            <li  class="auth-list-item"><a href="signup.php" class="header_name">Register</a></li>
-        </ul>
     </div>
 </div>
