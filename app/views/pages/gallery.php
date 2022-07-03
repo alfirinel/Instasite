076aa3948b438f58ad462b7a501ae271e19fcc0b
Alfirinel <dalnimdeluna@gmail.com>
2022-07-04 02:31:52 +0300

    gallery view css

--- app/views/pages/gallery.php
+++ app/views/pages/gallery.php
@@ -1,16 +1,21 @@
-<div class="galleryWrap">
+<?php ?>
+    <div class="galleryWrap">
         <?php if(count($photos) > 0):?>
             <?php foreach ($photos as $photo):?>
-                    <?= $photo['user'] ?>
-                    <img src="<?= $photo['path']?>" class="gallery-photo" alt="gallery-photo"/>
-                        <form action="/index/like" method="post" class="btn-like">
-                            <input type="hidden" name="id" value="<?= $photo['id']?>">
-                            <input type="submit" value="like">
-                            <?= $photo['likes']?>
-                            <?= $photo['created_at']?>
-                        </form>
+                <div class="grid">
+                    <div class="user-name"><?= $photo['user'] ?></div>
+                <img src="<?= $photo['path']?>" alt="gallery-photo" class="galleryPic"/>
+                    <div class="bottom-list">
+                        <div class="like-wrap">
+                            <form action="/index/like" method="post" class="btn-like">
+                                <input type="hidden" name="id" value="<?= $photo['id']?>">
+                                <input type="image" src="/images/like.png" class="like">
+                            </form>
+                            <div class="like-number"><?= $photo['likes']?></div>
+                        </div>
+                        <?= $photo['created_at']?>
+                    </div>
+                </div>
             <?php endforeach;?>
         <?php endif;?>
-</div>
-
-
+    </div>
