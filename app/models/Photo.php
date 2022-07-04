cabfaa91d65cd3db879d1192b6427cf967b1fb55
Alfirinel <dalnimdeluna@gmail.com>
2022-07-04 04:05:13 +0300

    update photo model all()

--- app/models/Photo.php
+++ app/models/Photo.php
@@ -7,7 +7,7 @@ use app\core\AbstractModel;
 class Photo extends AbstractModel
 {
     public function all(){
-        $sql = "SELECT photos.id as id, photos.path as path, photos.user_id as user_id, photos.likes as likes, photos.created_at as created_at, users.login as user FROM photos INNER JOIN users ON photos.user_id = users.id;";
+        $sql = "SELECT photos.id as id, photos.path as path, photos.user_id as user_id, photos.likes as likes, photos.created_at as created_at, users.login as user FROM photos INNER JOIN users ON photos.user_id = users.id ORDER BY photos.created_at DESC;;";
         $result = $this->db->query($sql);
         //TODO error handling
         return $result->fetch_all(MYSQLI_ASSOC);
@@ -49,11 +49,9 @@ class Photo extends AbstractModel
         }
     }
 
-    public function users()
+    public function pages()
     {
-        $sql = "SELECT * FROM users";
-        $result = $this->db->query($sql);
-        return $result->fetch_all(MYSQLI_ASSOC);
+
     }
 
 }
